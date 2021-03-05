e547a51830361d71c8959b95864a69109374c75d
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-03-05 12:12:13 +0800

    compile

--- include/DataStructures/Graph.h
+++ include/DataStructures/Graph.h
@@ -7,27 +7,96 @@
 class Graph
 {
 public:
-    vector<vector<int>> G;
+    vector<Edge> *edgeset;
+    vector<int> *pointset;
     Graph()
     {
-
+        edgeset = new vector<Edge>;
+        pointset = new vector<int>;
     };
     ~Graph()
     {
-        
+        delete (edgeset);
+        delete (pointset);
     };
 
 public:
-    bool AddVertex(int vertex);
-    bool RemoveVertex(int vertex);
-    bool AddEdge(int vertex1, int vertex2);
-    bool RemoveEdge(int vertex1, int vertex2);
+    bool AddVertex(int vertex)
+    {
+        if (ContainsVertex(vertex))
+            return false;
+        else
+            pointset.push_back(vertex);
+        return true;
+    };
+    bool RemoveVertex(int vertex)
+    {
+        if (VertexPosInSet(vertex) != pointset.end())
+        {
+            pointset.erase(it);
+            return true;
+        }
+        return false;
+    };
+    bool AddEdge(int vertex1, int vertex2)
+    {
+        if (ContainsVertex(vertex1) && ContainsVertex(vertex2))
+        {
+            if (ContainsEdge(vertex1, vertex2))
+                return false;
+            else
+                edgeset.push_back(Edge(vertex1, vertex2));
+        }
+        return false;
+    };
+    bool RemoveEdge(int vertex1, int vertex2)
+    {
+        if (ContainsVertex(vertex1) && ContainsVertex(vertex2))
+        {
+            auto it = EdgePosInSet(vertex1, vertex2);
+            if (it != pointset.end())
+            {
+                edgeset.erase(it);
+                return true;
+            }
+        }
+        return false;
+    };
 
 public:
-    int CountVertices() const;
-    int CountEdges() const;
-    bool ContainsVertex(int vertex) const;
-    bool ContainsEdge(int vertex1, int vertex2) const;
+    auto VertexPosInSet(int vertex)
+    {
+        return std::find(pointset.begin(), pointset.end(), vertex);
+    };
+    auto EdgePosInSet(int vertex1, int vertex2)
+    {
+        for (auto it = edgeset.begin(); it != edgeset.end(); it++)
+        {
+            if (*it->GetSource() == vertex1 && *it->GetDestination() == vertex2)
+                return it;
+        }
+        return edgeset.end();
+    };
+    int CountVertices() const
+    {
+        return pointset.size();
+    };
+    int CountEdges() const
+    {
+        return edgeset.size();
+    };
+    bool ContainsVertex(int vertex) const
+    {
+        if (VertexPosInSet(vertex) != pointset.end())
+            return true;
+        return false;
+    };
+    bool ContainsEdge(int vertex1, int vertex2) const
+    {
+        if (EdgePosInSet(vertex1, vertex2) != edgeset.end())
+            return true;
+        return false;
+    };
     std::vector<int> GetVertices() const;
     std::vector<Edge> GetEdges() const;
     std::vector<Edge> GetIncomingEdges(int vertex) const;
