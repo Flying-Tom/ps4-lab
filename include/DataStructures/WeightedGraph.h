fe54c29d99a324c4b5b615c55e40d9a47d757c96
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-03-12 15:55:50 +0800

    compile

--- include/DataStructures/WeightedGraph.h
+++ include/DataStructures/WeightedGraph.h
@@ -26,7 +26,7 @@ public:
     {
         if (points.find(vertex) == points.end())
         {
-            point[vertex] = make_pair(vertex, 1);
+            points[vertex] = make_pair(vertex, 1);
             edges[vertex] = vector<pair<int, int>>();
             return true;
         }
