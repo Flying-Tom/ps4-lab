f258b12116d6fac11584560989fd11945397d5fc
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-03-13 10:21:44 +0800

    compile

--- tests/02-hello-world.cpp
+++ tests/02-hello-world.cpp
@@ -15,7 +15,7 @@ int main()
     assert(g.RemoveVertex(4) == true);
     assert(g.ContainsVertex(4) == false);
     assert(g.RemoveVertex(5) == false);
-
+    puts("1");
     assert(g.AddEdge(1, 2, 1) == true);
     assert(g.AddEdge(1, 3, 9) == true);
     assert(g.AddEdge(2, 5, 1) == false);
