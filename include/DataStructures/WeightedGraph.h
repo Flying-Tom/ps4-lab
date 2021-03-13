30fddb9aa9cb45261d233cc6fac8b2a8bbf0510c
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-03-13 15:44:42 +0800

    compile

--- include/DataStructures/WeightedGraph.h
+++ include/DataStructures/WeightedGraph.h
@@ -92,7 +92,6 @@ public:
             return false;
 
         auto it = edges.find(vertex1);
-
         for (auto vit = it->second.begin(); vit != it->second.end(); vit++)
         {
             if (vit->first == vertex2)
--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -14,14 +14,17 @@ int main()
     assert(g.RemoveVertex(4) == true);
     assert(g.ContainsVertex(4) == false);
     assert(g.RemoveVertex(5) == false);
+    assert(g.AddVertex(5) == true);
 
     assert(g.AddEdge(1, 2) == true);
     assert(g.AddEdge(1, 3) == true);
     assert(g.AddEdge(2, 5) == false);
+    assert(g.AddEdge(5, 5) == true);
     assert(g.ContainsEdge(1, 2) == true);
     assert(g.ContainsEdge(2, 5) == false);
 
     assert(g.GetDegree(1) == 2);
+    assert(g.GetDegree(5) == 2);
     assert(g.GetIncomingEdges(1).empty());
     assert(g.GetOutgoingEdges(1).size() == 2);
     assert(g.GetNeighbors(1).size() == 2);
