7b9d820b321d9546acc1e45508bceed0863c9369
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-03-20 00:22:00 +0800

    compile

--- tests/03-hello-world.cpp
+++ tests/03-hello-world.cpp
@@ -36,7 +36,12 @@ int main()
     assert(g.ContainsEdge(2, 5) == false);
 
     assert(g.GetDegree(1) == 2);
-    assert(g.GetOutgoingEdges(1).size() == 3);
+    assert(g.GetOutgoingEdges(1).size() == 2);
+
+    assert(g.AddEdge(1, 1) == false);
+    assert(g.GetDegree(1) == 4);
+    assert(g.GetOutgoingEdges(1).size() == 4);
+
     assert(g.GetNeighbors(1).size() == 3);
 
     puts("Pass!");
