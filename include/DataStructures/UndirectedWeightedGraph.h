385071b9f00ec06de94eefaba92d4e783ae5eb62
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-03-19 22:12:53 +0800

    compile

--- include/DataStructures/UndirectedWeightedGraph.h
+++ include/DataStructures/UndirectedWeightedGraph.h
@@ -8,169 +8,88 @@ using namespace std;
 class UndirectedWeightedGraph : public WeightedGraph
 {
 private:
-
 public:
-    WeightedGraph(){
+    UndirectedWeightedGraph(){
 
     };
-    ~WeightedGraph(){
+    ~UndirectedWeightedGraph(){
 
     };
 
 public:
     bool AddVertex(int vertex)
     {
-        if (!ContainsVertex(vertex))
-        {
-            vertexs.insert(make_pair(vertex, 1));
-            edges.insert(make_pair(vertex, vector<pair<int, int>>()));
-            return true;
-        }
-        return false;
+        return WeightedGraph::AddVertex(vertex);
     };
     bool RemoveVertex(int vertex)
     {
-        if (ContainsVertex(vertex))
-        {
-            for (auto it : vertexs)
-                RemoveEdge(it.first, vertex);
-            edges.erase(edges.find(vertex));
-            vertexs.erase(vertexs.find(vertex));
-            return true;
-        }
-        return false;
+        return WeightedGraph::RemoveVertex(vertex);
     };
     bool AddEdge(int vertex1, int vertex2, int weight)
     {
-        if (ContainsVertex(vertex1) && ContainsVertex(vertex2) && !ContainsEdge(vertex1, vertex2))
-        {
-            edges[vertex1].emplace_back(make_pair(vertex2, weight));
-            return true;
-        }
-        return false;
+        return WeightedGraph::AddEdge(vertex1, vertex2, weight) && WeightedGraph::AddEdge(vertex2, vertex1, weight);
     };
     bool RemoveEdge(int vertex1, int vertex2)
     {
-        if (ContainsVertex(vertex1) && ContainsVertex(vertex2) && ContainsEdge(vertex1, vertex2))
-        {
-            auto it = edges.find(vertex1);
-            for (auto vit = it->second.begin(); vit != it->second.end(); vit++)
-            {
-                if (vit->first == vertex2)
-                {
-                    it->second.erase(vit);
-                    return true;
-                }
-            }
-        }
-        return false;
+        return WeightedGraph::RemoveEdge(vertex1, vertex2) && WeightedGraph::RemoveEdge(vertex2, vertex1);
     };
 
 public:
     int CountVertices() const
     {
-        return vertexs.size();
+        return WeightedGraph::CountVertices();
     };
     int CountEdges() const
     {
-        int res = 0;
-        for (auto it : edges)
-            res += it.second.size();
-        return res;
+        return WeightedGraph::CountEdges() / 2;
     };
     bool ContainsVertex(int vertex) const
     {
-        if (vertexs.find(vertex) != vertexs.end())
-            return true;
-        return false;
+        return WeightedGraph::ContainsVertex(vertex);
     };
     bool ContainsEdge(int vertex1, int vertex2) const
     {
-        if (ContainsVertex(vertex1) && ContainsVertex(vertex2))
-        {
-            auto it = edges.find(vertex1);
-            for (auto j : it->second)
-            {
-                if (j.first == vertex2)
-                    return true;
-            }
-        }
-        return false;
+        return WeightedGraph::ContainsEdge(vertex1, vertex2);
     };
     int GetWeight(int vertex1, int vertex2) const
     {
-        if (ContainsEdge(vertex1, vertex2))
-        {
-            auto it = edges.find(vertex1);
-            for (auto j : it->second)
-            {
-                if (j.first == vertex2)
-                    return j.second;
-            }
-        }
-        return -1;
+        return WeightedGraph::GetWeight(vertex1, vertex2);
     };
     vector<int> GetVertices() const
     {
-        vector<int> temp;
-        for (auto &i : vertexs)
-            temp.emplace_back(i.first);
-        return temp;
+        return WeightedGraph::GetVertices();
     };
     vector<WeightedEdge> GetEdges() const
     {
-        vector<WeightedEdge> temp;
-        for (auto i : edges)
-            for (auto j : i.second)
-                temp.emplace_back(WeightedEdge(i.first, j.first, j.second));
-        return temp;
+        vector<WeightedEdge> temp = Graph::GetEdges();
+        vector<WeightedEdge> res;
+        map<pair<int, int>, int> mtemp;
+
+        for (auto it = temp.begin(); it != temp.end(); it++)
+        {
+            int src = it->GetSource(), des = it->GetDestination(), weight = it->GetWeight();
+            if (mtemp.find(make_pair(src, des)) != mtemp.end() || mtemp.find(make_pair(des, src)) != mtemp.end())
+                continue;
+            res.emplace_back(Edge(src, des, weight));
+            mtemp.insert(make_pair(make_pair(src, des, weight), 1));
+        };
+        return res;
     };
     vector<WeightedEdge> GetIncomingEdges(int vertex) const
     {
-        vector<WeightedEdge> temp;
-        if (ContainsVertex(vertex))
-        {
-            for (auto i : edges)
-            {
-                for (auto j : i.second)
-                {
-                    if (j.first == vertex)
-                        temp.emplace_back(WeightedEdge(i.first, j.first, j.second));
-                }
-            }
-        }
-        return temp;
+        return WeightedGraph::GetIncomingEdges(vertex);
     };
     vector<WeightedEdge> GetOutgoingEdges(int vertex) const
     {
-        vector<WeightedEdge> temp;
-        if (ContainsVertex(vertex))
-        {
-            auto it = edges.find(vertex);
-            for (auto vit : it->second)
-                temp.emplace_back(WeightedEdge(it->first, vit.first, vit.second));
-        }
-        return temp;
+        return WeightedGraph::GetOutgoingEdges(vertex);
     };
     int GetDegree(int vertex) const
     {
-        if (ContainsVertex(vertex))
-        {
-            auto it = edges.find(vertex);
-            return it->second.size();
-        }
-        return 0;
+        return WeightedGraph::GetDegree(vertex);
     };
     vector<int> GetNeighbors(int vertex) const
     {
-        vector<int> temp;
-        if (ContainsVertex(vertex))
-        {
-            auto it = edges.find(vertex);
-            for (auto vit : it->second)
-                temp.emplace_back(vit.first);
-        }
-        return temp;
+        return WeightedGraph::GetNeighbors(vertex);
     };
 };
 
