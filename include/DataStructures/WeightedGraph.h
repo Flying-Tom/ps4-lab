d5a4a1fc2330f497bdf90f9460c4730d997a55e1
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-03-28 15:51:09 +0800

    compile

--- include/DataStructures/WeightedGraph.h
+++ include/DataStructures/WeightedGraph.h
@@ -20,65 +20,65 @@ public:
 public:
     bool AddVertex(int vertex)
     {
-        return Generic::AddVertex(vertex);
+        return Generic<WeightedEdge<T>>::AddVertex(vertex);
     };
     bool RemoveVertex(int vertex)
     {
-        return Generic::RemoveVertex(vertex);
+        return Generic<WeightedEdge<T>>::RemoveVertex(vertex);
     };
     bool AddEdge(int vertex1, int vertex2, int weight)
     {
-        return Generic::InsertEdge(WeightedEdge(vertex1, vertex2, weight));
+        return Generic<WeightedEdge<T>>::InsertEdge(WeightedEdge(vertex1, vertex2, weight));
     };
     bool RemoveEdge(int vertex1, int vertex2)
     {
-        return Generic::RemoveEdge(vertex1, vertex2);
+        return Generic<WeightedEdge<T>>::RemoveEdge(vertex1, vertex2);
     };
 
 public:
     int CountVertices() const
     {
-        return Generic::CountVertices();
+        return Generic<WeightedEdge<T>>::CountVertices();
     };
     int CountEdges() const
     {
-        return Generic::CountEdges();
+        return Generic<WeightedEdge<T>>::CountEdges();
     };
     bool ContainsVertex(int vertex) const
     {
-        return Generic::ContainsVertex(vertex);
+        return Generic<WeightedEdge<T>>::ContainsVertex(vertex);
     };
     bool ContainsEdge(int vertex1, int vertex2) const
     {
-        return Generic::ContainsEdge(vertex1, vertex2);
+        return Generic<WeightedEdge<T>>::ContainsEdge(vertex1, vertex2);
     };
     int GetWeight(int vertex1, int vertex2) const
     {
-        return Generic::GetWeight(vertex1, vertex2);
+        return Generic<WeightedEdge<T>>::GetWeight(vertex1, vertex2);
     };
     vector<int> GetVertices() const
     {
-        return Generic::GetVertices();
+        return Generic<WeightedEdge<T>>::GetVertices();
     };
     vector<WeightedEdge<T>> GetEdges() const
     {
-        return Generic::GetEdges();
+        return Generic<WeightedEdge<T>>::GetEdges();
     };
     vector<WeightedEdge<T>> GetIncomingEdges(int vertex) const
     {
-        return Generic::GetIncomingEdges(vertex);
+        return Generic<WeightedEdge<T>>::GetIncomingEdges(vertex);
     };
     vector<WeightedEdge<T>> GetOutgoingEdges(int vertex) const
     {
-        return Generic::GetOutgoingEdges(vertex);
+        return Generic<WeightedEdge<T>>::GetOutgoingEdges(vertex);
     };
     int GetDegree(int vertex) const
     {
-        return Generic::GetDegree(vertex);
+        return Generic<WeightedEdge<T>>::GetDegree(vertex);
     };
     vector<int> GetNeighbors(int vertex) const
     {
-        return Generic::GetNeighbors(vertex);
+        return Generic<WeightedEdge<T>>::GetNeighbors(vertex);
     };
 };
 
