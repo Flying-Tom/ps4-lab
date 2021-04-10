f8f546963b10cf4fe23caede63731f9567c6d1b7
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-10 12:38:59 +0800

    compile

--- include/Algorithms/BreadthFirstSearcher.h
+++ include/Algorithms/BreadthFirstSearcher.h
@@ -3,12 +3,53 @@
 
 #include <functional>
 #include <optional>
+#include <set>
+#include <queue>
 
 template <typename TGraph>
-class BreadthFirstSearcher {
- public:
-  static void VisitAllVertices(const TGraph *graph, int start, const std::function<void(int)> &action);
-  static std::optional<int> FindFirstVertex(const TGraph *graph, int start, const std::function<bool(int)> &predicate);
+class BreadthFirstSearcher
+{
+public:
+    static void VisitAllVertices(const TGraph *graph, int start, const std::function<void(int)> &action)
+    {
+        set<int> vis;
+        queue<int> q{start};
+        if (graph->ContainsVertex(start))
+        {
+            while (!q.empty())
+            {
+                int cur = q.front(), q.pop();
+                action(cur);
+                vis.emplace(cur);
+                for (int u : graph->GetNeighbors(cur))
+                {
+                    if (!vis.contains(u))
+                        q.emplace(u);
+                }
+            }
+        }
+    };
+    static std::optional<int> FindFirstVertex(const TGraph *graph, int start, const std::function<bool(int)> &predicate)
+    {
+        set<int> vis;
+        queue<int> q{start};
+        if (graph->ContainsVertex(start))
+        {
+            while (!q.empty())
+            {
+                int cur = q.front(), q.pop();
+                if (predicate(cur))
+                    return cur;
+                vis.emplace(cur);
+                for (int u : graph->GetNeighbors(cur))
+                {
+                    if (!vis.contains(u))
+                        q.emplace(u);
+                }
+            }
+        }
+        return std::nullopt;
+    };
 };
 
 #endif
--- include/Algorithms/DepthFirstSearcher.h
+++ include/Algorithms/DepthFirstSearcher.h
@@ -3,12 +3,53 @@
 
 #include <functional>
 #include <optional>
+#include <set>
+#include <stack>
 
 template <typename TGraph>
-class DepthFirstSearcher {
- public:
-  static void VisitAllVertices(const TGraph *graph, int start, const std::function<void(int)> &action);
-  static std::optional<int> FindFirstVertex(const TGraph *graph, int start, const std::function<bool(int)> &predicate);
+class DepthFirstSearcher
+{
+public:
+    static void VisitAllVertices(const TGraph *graph, int start, const std::function<void(int)> &action)
+    {
+        set<int> vis;
+        stack<int> s{start};
+        if (graph->ContainsVertex(start))
+        {
+            while (!s.empty())
+            {
+                int cur = s.top(), s.pop();
+                action(cur);
+                vis.emplace(cur);
+                for (int u : graph->GetNeighbors(cur))
+                {
+                    if (!vis.contains(u))
+                        s.emplace(u);
+                }
+            }
+        }
+    };
+    static std::optional<int> FindFirstVertex(const TGraph *graph, int start, const std::function<bool(int)> &predicate)
+    {
+        set<int> vis;
+        stack<int> s{start};
+        if (graph->ContainsVertex(start))
+        {
+            while (!s.empty())
+            {
+                int cur = s.top(), s.pop();
+                if (predicate(cur))
+                    return cur;
+                vis.emplace(cur);
+                for (int u : graph->GetNeighbors(cur))
+                {
+                    if (!vis.contains(u))
+                        s.emplace(u);
+                }
+            }
+        }
+        return std::nullopt;
+    };
 };
 
 #endif
