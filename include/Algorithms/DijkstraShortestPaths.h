35741361be120fadbedfe4a2e9119ee149198c99
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-18 14:51:36 +0800

    compile

--- include/Algorithms/DijkstraShortestPaths.h
+++ include/Algorithms/DijkstraShortestPaths.h
@@ -3,40 +3,14 @@
 
 #include <Algorithms/ShortestPaths.h>
 
-class DijkstraShortestPaths 
+template <template <typename> class TGraph, typename TValue>
+class DijkstraShortestPaths ::ShortestPaths<TGraph, TValue>
 {
 public:
     ShortestPaths() = delete;
 
-    ShortestPaths(const TGraph<TValue> *graph, int source)
-    {
-        map<int, TValue> cost;
-        map<int, bool> vis;
+    ShortestPaths(const TGraph<TValue> *graph, int source){
 
-        priority_queue<pair<TValue, int>> Q;
-        cost[source] = 0;
-        Q.emplace(0, source);
-        while (!Q.empty())
-        {
-            const auto state = Q.top();
-            Q.pop();
-            const TValue cur_cost = state.first;
-            const int cur_idx = state.second;
-            if (vis[cur_idx])
-                continue;
-            vis[cur_idx] = true;
-            vector<int> neighbors;
-            for (const auto &edge : graph->GetOutgoingEdges())
-            {
-                const TValue new_cost = cur_cost - edge.GetWeight();
-                const int new_idx = edge.GetDestination();
-                if (new_cost > cost[new_idx])
-                {
-                    Q.push({new_cost, new_idx});
-                    cost[new_idx] = new_cost;
-                }
-            }
-        }
     };
 
     virtual ~ShortestPaths();
@@ -44,17 +18,17 @@ public:
 public:
     bool HasPathTo(int destination) const
     {
-        return false;
+        return ShortestPaths::HasPathto(destination);
     };
 
     std::optional<TValue> TryGetDistanceTo(int destination) const
     {
-        return std::nullopt;
+        return ShortestPaths::TryGetDistanceTo(destination);
     };
 
     std::optional<std::vector<int>> TryGetShortestPathTo(int destination) const
     {
-        return std::nullopt;
+        return ShortestPaths::TryGetShortestPathTo(destination);
     };
 };
 
