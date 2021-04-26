f998afac8d562a7ed27740a2f5063388905ff2f6
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-26 15:15:04 +0800

    compile

--- include/Algorithms/ShortestPaths.h
+++ include/Algorithms/ShortestPaths.h
@@ -13,7 +13,7 @@ class ShortestPaths
 {
 
 public:
-    static_assert(is_detected(typename TGraph::value_type), "TGraph should be weighted");
+    static_assert(is_same<TGraph, WeightedGraph<TValue>>::value || is_same<TGraph, UndirectedWeightedGraph<TValue>>::value, "TGraph should be weighted");
     typedef typename TGraph::value_type TValue;
     mutable map<int, TValue> cost;
     mutable map<int, int> parent;
