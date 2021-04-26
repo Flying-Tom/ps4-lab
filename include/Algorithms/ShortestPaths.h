b2462268f55a3548c460b3ecc0ffb78dc046f115
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-26 15:45:34 +0800

    compile

--- include/Algorithms/ShortestPaths.h
+++ include/Algorithms/ShortestPaths.h
@@ -8,6 +8,8 @@
 #include <DataStructures/Graph.h>
 #include <DataStructures/UndirectedGraph.h>
 
+#include <iostream>
+
 template <typename TGraph>
 class ShortestPaths
 {
@@ -15,7 +17,7 @@ class ShortestPaths
 public:
     static_assert(!(is_same<TGraph, Graph>::value || is_same<TGraph, UndirectedGraph>::value), "TGraph should be weighted");
     typedef typename TGraph::value_type TValue;
-    std::cout << decltype(TValue);
+    cout << decltype(TValue);
     static_assert(is_default_constructible<TValue>::value, "TValue requires default constructor");
     mutable map<int, TValue> cost;
     mutable map<int, int> parent;
