01d8a166f6ea2cf3811eed59078e6d242791eef0
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-26 15:32:04 +0800

    compile

--- include/Algorithms/ShortestPaths.h
+++ include/Algorithms/ShortestPaths.h
@@ -15,6 +15,8 @@ class ShortestPaths
 public:
     static_assert(!(is_same<TGraph, Graph>::value || is_same<TGraph, UndirectedGraph>::value), "TGraph should be weighted");
     typedef typename TGraph::value_type TValue;
+
+    static_assert((is_default_constructible<TValue>::value,"TValue requires default constructor");
     mutable map<int, TValue> cost;
     mutable map<int, int> parent;
     int src;
