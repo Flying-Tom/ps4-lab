a4d25d136c85a0a1cca408fb497ad8d995eba7fa
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-05-09 02:08:17 +0800

    compile

--- include/Algorithms/epsilon.h
+++ include/Algorithms/epsilon.h
@@ -2,12 +2,6 @@
 #define EPSILON_H
 
 template <typename TValue>
-TValue epsilon()
-{
-    return epsilon_real<TValue>(std::is_floating_point<TValue>());
-}
-
-template <typename TValue>
 TValue epsilon_real(std::true_type)
 {
     return 1e-5;
@@ -17,5 +11,11 @@ template <typename TValue>
 TValue epsilon_real(std::false_type)
 {
     return TValue();
+}
+
+template <typename TValue>
+TValue epsilon()
+{
+    return epsilon_real<TValue>(std::is_floating_point<TValue>());
 }
 #endif
