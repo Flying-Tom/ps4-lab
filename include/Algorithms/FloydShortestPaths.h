9e6b4edd84dc04653bfa97f684a8e221f039e9e1
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-05-08 16:33:59 +0800

    compile

--- include/Algorithms/FloydShortestPaths.h
+++ include/Algorithms/FloydShortestPaths.h
@@ -8,8 +8,6 @@ class FloydShortestPaths : public MultiSourceShortestPaths<TGraph>
 {
     typedef typename MultiSourceShortestPaths<TGraph>::TValue TValue;
 
-#define MultiSourceShortestPaths <TGraph>::cost MultiSourceShortestPaths<TGraph>::MultiSourceShortestPaths<TGraph>::cost
-
 public:
     FloydShortestPaths() = delete;
 
