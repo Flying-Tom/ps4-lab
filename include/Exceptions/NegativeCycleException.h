8ed42a4f2409d60f125fa648b8eb91b27ff3c958
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-05-08 15:18:33 +0800

    compile

--- include/Exceptions/NegativeCycleException.h
+++ include/Exceptions/NegativeCycleException.h
@@ -9,11 +9,14 @@ protected:
     std::string message;
 
 public:
-    explicit NegativeCycleException(const std::string &s) : GLException(s){};
+    explicit NegativeCycleException(const std::string &s) : GLException(s)
+    {
+        message = s
+    }
     virtual std::string GetMessage() const
     {
         return string();
-    };
+    }
 };
 
 #endif
