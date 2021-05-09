90af86043a5ae50a65f32e4e3a393204116ab1de
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-05-09 12:40:31 +0800

    compile

--- tests/07-floyd.cpp
+++ tests/07-floyd.cpp
@@ -4,10 +4,6 @@
 #include <Algorithms/FloydShortestPaths.h>
 using namespace std;
 #define TYPE int
-void func(int x)
-{
-    printf("%d ", x);
-}
 
 int main()
 {
@@ -15,27 +11,9 @@ int main()
 
     g->AddVertex(1);
 
-    g->AddEdge(1, 1, 1);
-    /*
-    g->AddEdge(2, 3, 2);
-    g->AddEdge(3, 1, -4);
-
-    g->AddEdge(1, 1, 11);
-    g->AddEdge(2, 4, 61);
-    g->AddEdge(3, 10, 1);
-    g->AddEdge(3, 11, 61);
-    g->AddEdge(4, 12, 21);
-    g->AddEdge(1, 5, 11);
-    g->AddEdge(5, 6, 13);
-    g->AddEdge(6, 7, 13);
-    g->AddEdge(6, 8, 12);
-    g->AddEdge(5, 9, 13);
-    g->AddEdge(7, 8, 11);
-    */
+    g->AddEdge(1, 1, -1);
 
     puts("=======ShortestPaths =======");
     MultiSourceShortestPaths<WeightedGraph<TYPE>> *p = new FloydShortestPaths<WeightedGraph<TYPE>>(g);
-    //ShortestPaths<WeightedGraph<TYPE>> *q = new BellmanFordShortestPaths<WeightedGraph<TYPE>>(g, 1);
-
     puts("\n=======================");
 }
