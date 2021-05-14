769d42ff7a1d10d5bb1d47523a300edbb41f316d
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-05-14 17:43:06 +0800

    compile

--- include/Algorithms/BiparititeGraphMatching.h
+++ include/Algorithms/BiparititeGraphMatching.h
@@ -2,13 +2,47 @@
 #define GRAPHLIBRARY_BIPARTITEGRAPHMATCHING_H
 #include <unordered_set>
 #include <optional>
+#include <map>
 
-template<typename TGraph>
-class BipariteGraphMatching {
+template <typename TGraph>
+class BipariteGraphMatching
+{
+    map<int, bool> used;
+    map<int, int> belong;
+
+private:
+    bool find(const TGraph *g, int x)
+    {
+        for (auto e : g->GetOutgoingEdges(x))
+        {
+            int y = e.GetDestination();
+            if (used.find(y) == used.end())
+            {
+                used[y] = true;
+                if (belong.find(y) == belong.end() || find(belong.find(y)))
+                {
+                    belong[y] = x;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void hungarian(){
+
+    };
+
 public:
-    BipariteGraphMatching(const TGraph* g, std::unordered_set<int> left, std::unordered_set<int> right);
-    int SumOfMatches() const;
-    std::optional<int> FindMatchOf(int idx) const;
+    BipariteGraphMatching(const TGraph *g, std::unordered_set<int> left, std::unordered_set<int> right){
+
+    };
+    int SumOfMatches() const {
+
+    };
+    std::optional<int> FindMatchOf(int idx) const {
+
+    };
 };
 
 #endif //GRAPHLIBRARY_BIPARTITEGRAPHMATCHING_H
---
+++ tests/08-Biparite.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cassert>
+#include <DataStructures/UndirectedGraph.h>
+#include <DataStructures/UndirectedWeightedGraph.h>
+#include <Algorithms/BiparititeGraphMatching.h>
+using namespace std;
+#define TYPE int
+
+int main()
+{
+    UndirectedWeightedGraph<TYPE> *g = new UndirectedWeightedGraph<TYPE>;
+
+    g->AddVertex(1);
+    g->AddVertex(2);
+    g->AddVertex(3);
+    g->AddVertex(4);
+    g->AddVertex(5);
+    g->AddVertex(6);
+    g->AddVertex(7);
+    g->AddVertex(8);
+    g->AddVertex(9);
+    g->AddVertex(10);
+    g->AddVertex(11);
+    g->AddVertex(12);
+
+    g->AddEdge(1, 2, 1);
+    g->AddEdge(1, 1, 11);
+    g->AddEdge(2, 3, 12);
+    g->AddEdge(2, 4, 61);
+    g->AddEdge(3, 10, 1);
+    g->AddEdge(3, 11, 61);
+    g->AddEdge(4, 12, 21);
+    g->AddEdge(1, 5, 11);
+    g->AddEdge(5, 6, 13);
+    g->AddEdge(6, 7, 13);
+    g->AddEdge(6, 8, 12);
+    g->AddEdge(5, 9, 13);
+    g->AddEdge(7, 8, 11);
+
+    puts("======= BiparititeGraphMatching =======");
+    BipariteGraphMatching<WeightedGraph<TYPE>> *p = new BipariteGraphMatching<WeightedGraph<TYPE>>(g);
+    cout << p->TryGetDistanceOf(1, 1).value();
+    puts("\n=======================");
+}
