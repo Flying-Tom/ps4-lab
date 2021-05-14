3243ee7d295b246aad3dae165b954801081d4f97
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-05-14 18:42:29 +0800

    compile

--- tests/08-Biparite.cpp
+++ tests/08-Biparite.cpp
@@ -9,6 +9,7 @@ using namespace std;
 int main()
 {
     UndirectedWeightedGraph<TYPE> *g1 = new UndirectedWeightedGraph<TYPE>;
+    UndirectedGraph *g2 = new UndirectedGraph;
 
     g1->AddVertex(1);
     g1->AddVertex(2);
@@ -23,13 +24,27 @@ int main()
     g1->AddVertex(11);
     g1->AddVertex(12);
 
+    g2->AddVertex(1);
+    g2->AddVertex(2);
+    g2->AddVertex(3);
+    g2->AddVertex(4);
+    g2->AddVertex(5);
+    g2->AddVertex(6);
+    g2->AddVertex(7);
+    g2->AddVertex(8);
+    g2->AddVertex(9);
+    g2->AddVertex(10);
+    g2->AddVertex(11);
+    g2->AddVertex(12);
+
     g1->AddEdge(1, 5, 1);
     g1->AddEdge(2, 6, 1);
     g1->AddEdge(3, 7, 1);
     g1->AddEdge(4, 8, 1);
 
     puts("======= BiparititeGraphMatching =======");
-    BipariteGraphMatching<WeightedGraph<TYPE>> *p = new BipariteGraphMatching<WeightedGraph<TYPE>>(g1, {1, 2, 3, 4}, {5, 6, 7, 8});
+    BipariteGraphMatching<UndirectedWeightedGraph<TYPE>> *p = new BipariteGraphMatching<UndirectedWeightedGraph<TYPE>>(g1, {1, 2, 3, 4}, {5, 6, 7, 8});
+    BipariteGraphMatching<UndirectedGraph> *q = new BipariteGraphMatching<UndirectedGraph>(g1, {1, 2, 3, 4}, {5, 6, 7, 8});
 
     cout << p->SumOfMatches() << endl;
     cout << p->FindMatchOf(1).value() << endl;
